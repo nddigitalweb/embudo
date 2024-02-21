93ed7a934c90985f7ba7b616c755db01ec3e851e
joaquin <joacodicio@gmail.com>
2024-02-21 14:04:50 -0300

    validations

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ function generateForm() {
   );
   const a = createElement("a", "submit-form", "main");
   a.textContent = "Enviar";
+  a.setAttribute('target', '_blank')
   form.appendChild(a);
   return form;
 }
@@ -57,7 +58,7 @@ function generateForm() {
 function updateHref() {
   const template = `https://api.whatsapp.com/send?phone=+541126655209&text=QUIERO+MI+ANALISIS+%F0%9F%94%A5%0A%0A${company}+%F0%9F%8F%AD%0A${email}+%E2%9C%89%EF%B8%8F%0A${employees}+%F0%9F%A7%91`;
   const submit = document.getElementById("submit-form");
-  submit.setAttribute("href", template);
+  if (company.trim() != '' && email.trim() != '' && employees.trim() != '') submit.setAttribute("href", template);
 }
 //utilities
 
